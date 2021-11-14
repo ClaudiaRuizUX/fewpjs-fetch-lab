967acb44bcc654712e4e180d770c95c110d041bd
Claudia <claudiaruizux@gmail.com>
2021-11-15 09:24:21 +1100

    Completed Fetch Lab

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+
+// fetch('https://anapioficeandfire.com/api/books')
+// .then(resp => resp.json())
+// .then(json => console.log(json));
+
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books")
+  return fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
     .then(json => renderBooks(json))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(json) {
+
+  console.log(json);
+  // const main = document.querySelector('main');
+  // books.forEach(book => {
+  //   const h2 = document.createElement('h2');
+  //   h2.innerHTML = book.name;
+  //   main.appendChild(h2);
+  // });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
